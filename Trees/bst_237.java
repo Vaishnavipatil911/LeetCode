554662a9d167ff2d5fee55524eac120e12b6393e
Vaishnavi Patil <vaishnavivpatil911@gmail.com>
2026-08-17 14:37:16 +0530

    237

---
+++ Trees/bst_237.java
@@ -0,0 +1,39 @@
+package Trees;
+
+public class bst_237 {
+
+ // Definition for a binary tree node.
+ public class TreeNode {
+     int val;
+     TreeNode left;
+     TreeNode right;
+     TreeNode(int x) { val = x; }
+ }
+ 
+
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root==null || p==null || q==null)
+        return null;
+        TreeNode current=root;
+        while(current!=null)
+        {
+            if(p.val<current.val && q.val<current.val)
+            {
+                current=current.left;
+            }
+            else if(p.val>current.val && q.val>current.val)
+            {
+                current=current.right;
+            }
+            else
+            {
+                return current;
+            }
+
+        }
+        return null;
+        
+    }
+}
+}
