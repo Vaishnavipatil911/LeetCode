536333b84383154dee6b308436a0d6d34d2ba041
Vaishnavi Patil <vaishnavivpatil911@gmail.com>
2026-08-23 23:29:50 +0530

    lc_46

---
+++ Backtracking/permutation.java
@@ -0,0 +1,33 @@
+package Backtracking;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class permutation {
+    class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>>resultlist=new ArrayList<>();
+        backtrack(resultlist,new ArrayList<>(),nums);
+        return resultlist;
+        
+    }
+    public void backtrack(List<List<Integer>>resultlist,ArrayList<Integer>templist,int[]nums)
+    {
+        
+        if(templist.size()==nums.length)
+        {resultlist.add(new ArrayList<>(templist));
+        }
+        for(int number : nums)
+        {
+        if(templist.contains(number))
+       { continue;
+       }
+       templist.add(number);
+
+        backtrack(resultlist ,templist, nums);
+
+        templist.remove(templist.size()-1);
+        }
+    }
+}
+}
