136bdeed9974ddcb39318d113425723c0f9d16ac
Vaishnavi Patil <vaishnavivpatil911@gmail.com>
2026-08-27 23:23:32 +0530

    11

---
+++ Leetcode 75/lc_11.java
@@ -0,0 +1,24 @@
+public class lc_11 {
+    class Solution {
+    public int maxArea(int[] height) {
+        int left=0;
+        int right=height.length-1;
+        int maxArea=0;
+        while(left<right)
+        {
+            int currentArea=(right-left)*Math.min(height[left],height[right]);
+            maxArea=Math.max(maxArea,currentArea);
+            if(height[left]<height[right])
+            {
+                left++;
+            }
+            else
+            {
+                right--;
+            }
+        }
+        return maxArea;
+        
+    }
+}
+}
