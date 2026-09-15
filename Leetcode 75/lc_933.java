b6d76ec0d678762c10c7871b8ea04818a4860fad
Vaishnavi Patil <vaishnavivpatil911@gmail.com>
2026-09-15 23:43:13 +0530

    lc_933

---
+++ Leetcode 75/lc_933.java
@@ -0,0 +1,28 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class lc_933 {
+    class RecentCounter {
+    Queue<Integer> queue;
+    public RecentCounter() {
+        queue= new LinkedList<>();
+        
+    }
+    
+    public int ping(int t) {
+        queue.add(t);
+        while(queue.peek()<t-3000)
+        {
+            queue.poll();
+        }
+        return queue.size();
+        
+    }
+}
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * RecentCounter obj = new RecentCounter();
+ * int param_1 = obj.ping(t);
+ */
+}
